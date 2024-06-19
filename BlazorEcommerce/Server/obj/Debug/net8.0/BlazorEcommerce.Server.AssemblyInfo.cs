a123ccb8bee397c203912c439a6cbe228d525503
Stefan Josanu <josanu399@gmail.com>
2024-06-19 22:27:55 +0300

    second commit

--- BlazorEcommerce/Server/obj/Debug/net8.0/BlazorEcommerce.Server.AssemblyInfo.cs
+++ BlazorEcommerce/Server/obj/Debug/net8.0/BlazorEcommerce.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEcommerce.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183b2c8b5cf6262a09cc4a32fa397ec39e5f0371")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEcommerce.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEcommerce.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
