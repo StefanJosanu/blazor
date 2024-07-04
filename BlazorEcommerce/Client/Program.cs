cfe6c6eb3b4cb9d002b31fedb9813742ffc25288
Cabac Ionel <ionel.cabac@codwer.com>
2024-07-04 13:50:32 +0300

    fix layout

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using MudBlazor.Services;
 using BlazorEcommerce.Client.Services.ProductService;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorEcommerce.Client.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +15,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<EcommerceAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<EcommerceAuthenticationStateProvider>());
 builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+
 builder.Services.AddScoped<IProductService, ProductService>();
+
 await builder.Build().RunAsync();
