abd81c2e11ec34e983ad8cf08809e87dbb024f7b
Stefan Josanu <josanu399@gmail.com>
2024-07-04 13:09:30 +0300

    mudlayout

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -9,33 +9,40 @@ namespace BlazorEcommerce.Server;
 public class ProductController : ControllerBase
 {
     private readonly IMediator _mediator;
+
     public ProductController(IMediator mediator)
     {
         _mediator = mediator;
     }
+
     [HttpGet("get-products")]
     public async Task<List<GetProductDTO>> GetAllProducts()
     {
         return await _mediator.Send(new GetProductsQuerry());
     }
+
     [HttpGet("get-id-product")]
     public async Task<Product> GetProductById(Guid id)
     {
         return await _mediator.Send(new GetProductByIdQuerry(id) { Id = id });
     }
+
     [HttpPost("create-product")]
     public async Task<Unit> CreateProduct([FromBody] ProductDTO createProductDTO)
     {
         return await _mediator.Send(new CreateProductCommand(createProductDTO));
     }
+
     [HttpPut("update-product")]
     public async Task<CreateProductDTO> UpdateProduct (Guid id, [FromBody] CreateProductDTO productDTO)
     {
         return await _mediator.Send(new UpdateProductCommand(id, productDTO) { Id = id });
     }
+
     [HttpDelete("delete-product")]
     public async Task<Unit> DeleteProduct(Guid id)
     {
         return await _mediator.Send( new RemoveProductCommand(id) { Id = id} );
     }
+
 }
