ff9da2da25a25a762ff6060275036e34594ed76a
Stefan Josanu <josanu399@gmail.com>
2024-08-15 10:55:12 +0300

    Dialog Create, Dialog Delete, Dialog Edit

--- BlazorEcommerce/Shared/ProductDTO.cs
+++ BlazorEcommerce/Shared/ProductDTO.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorEcommerce.Shared
 {
-    public class GetProductDTO
+    public class ProductDTO
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string Description { get; set; }
         [Column(TypeName = "decimal(18, 6)")]
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
-        public string StockLocationName { get; set; }
-        public string StockLocationAddress { get; set; }
         public Guid? StockLocationId { get; set; }
     }
 }
